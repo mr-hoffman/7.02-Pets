1ca86f6450e9d2a1ad4746e6e1142eb33bcb95c2
mr-hoffman <hoffmant@hilbertk12.org>
2025-04-01 10:39:27 -0500

    Create pets-class.py

---
+++ pets-class.py
@@ -0,0 +1,11 @@
+############### 7.02 Pets Class ##################
+
+### define your Pets class ###
+
+
+### instantiate several Pet objects ###
+### and store them in a list of Pets ###
+
+
+### print the name and at least one attribute for each Pet ###
+
